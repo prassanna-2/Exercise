cdd8440e7aa4d74c7d475c5cb6458791accaa230
prassanna-2 <114499427+prassanna-2@users.noreply.github.com>
2025-06-08 23:36:21 +0530

    Add files via upload

---
+++ Task2_3.java
@@ -0,0 +1,23 @@
+package Theme;
+
+import java.util.*;
+
+public class Task2_3 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a=sc.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even number");
+		}
+		else
+		{
+			System.out.println("odd number");
+		}
+
+	}
+
+}
