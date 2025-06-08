03a2424e34f370821a91b2ded0aff90462bcbf41
prassanna-2 <114499427+prassanna-2@users.noreply.github.com>
2025-06-08 23:37:15 +0530

    Add files via upload

---
+++ Task2_7.java
@@ -0,0 +1,16 @@
+package Theme;
+
+import java.util.*;
+
+public class Task2_7 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("10*"+i+"="+10*i);
+		}
+
+	}
+
+}
