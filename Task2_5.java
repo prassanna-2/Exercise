2d015f7f36ed31ade7fc0611fb7f1332e34db50f
prassanna-2 <114499427+prassanna-2@users.noreply.github.com>
2025-06-08 23:36:50 +0530

    Add files via upload

---
+++ Task2_5.java
@@ -0,0 +1,15 @@
+package Theme;
+
+import java.util.*;
+//sum of 10 to 50 numbers
+public class Task2_5 {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=10;i<=50;i++)
+		{
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+
+}
