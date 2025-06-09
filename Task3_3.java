db08ce93960949606383e79418398a9f644e458d
prassanna-2 <114499427+prassanna-2@users.noreply.github.com>
2025-06-09 16:04:49 +0530

    Add files via upload

---
+++ Task3_3.java
@@ -0,0 +1,24 @@
+package theme_2;
+
+import java.util.*;
+public class Task3_3 {
+
+	public static void main(String[] args) {
+		
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the first number");
+	float a=sc.nextFloat();
+	System.out.println("Enter the second number");
+	float b=sc.nextFloat();
+	if(a*100==b*100)
+	{
+		System.out.println("similar number");
+	}
+	else
+	{
+		System.out.println("not similar number");
+	}
+
+	}
+
+}
