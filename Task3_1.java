0111ff6f3399c8b633f4bbd34e253b7b0510035f
prassanna-2 <114499427+prassanna-2@users.noreply.github.com>
2025-06-09 16:03:12 +0530

    Add files via upload

---
+++ Task3_1.java
@@ -0,0 +1,17 @@
+package theme_2;
+
+import java.util.*;
+
+public class Task3_1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num=sc.nextInt();
+		System.out.println("Cube is:");
+		System.out.println(num*num*num);
+		
+
+	}
+
+}
