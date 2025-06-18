253e92b460a9928ec6c441294426009eb44af0cd
prassanna-2 <114499427+prassanna-2@users.noreply.github.com>
2025-06-18 19:25:33 +0530

    Add files via upload

---
+++ Task6_1.java
@@ -0,0 +1,18 @@
+package theme_2;
+
+public class Task6_1 {
+	static int a=10;
+	static int b=20;
+	int c;
+	static int add(int a,int b) {
+		return a+b;
+		
+	}
+	public static void main(String[] args) {
+		Task6_1 tr=new Task6_1();
+		c=add(a,b);
+		
+
+	}
+
+}
